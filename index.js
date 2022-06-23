39b3388e56bb11922ee68fadf7609fbdbd795cbd
jgood13 <joseph.good8@gmail.com>
2022-06-23 12:31:50 -0600

    added dev files, linked inquirer

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+const inuqirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./src/generateMarkdown.js')
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
